4935ee98876c65b41b5b4e3f87544993ffb3a950
Андреянов Денис Сергеевич <denis.andreyanov@vitech.team>
2024-05-13 23:09:38 +0300

    Fix after cr

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"regexp"
@@ -29,6 +31,11 @@ func (u *UserRoutes) RegisterUser(c *gin.Context) {
 		return
 	}
 	passwordRegex := regexp.MustCompile(`^[a-zA-Z0-9]{8,}$`)
+	loginRegex := regexp.MustCompile(`^[a-zA-Z0-9]{3,20}$`)
+	if !loginRegex.MatchString(user.Username) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Логин должен содержать только латиницу и цифры, от 3 до 20 символов"})
+		return
+	}
 	if !passwordRegex.MatchString(user.Password) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Пароль должен содержать только латиницу и цифры и быть не короче 8 символов"})
 		return
@@ -40,11 +47,18 @@ func (u *UserRoutes) RegisterUser(c *gin.Context) {
 		return
 	}
 	user.Password = strings.TrimRight(string(hashedPassword), "\n")
-
 	user.ID = uuid.New().String()
 	_, err = u.db.Exec("INSERT INTO users (id, username, password, email) VALUES ($1, $2, $3, $4)", user.ID, user.Username, user.Password, user.Email)
+
+	if sqliteErr, ok := err.(sqlite3.Error); ok {
+		if errors.Is(sqliteErr.ExtendedCode, sqlite3.ErrConstraintUnique) {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Логин уже занят"})
+			return
+		}
+	}
+
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Произошла ошибка при попытке зарегистрироваться. Попробуйте позже."})
 		return
 	}
 
